511e88f12b0fdcda0d45c367a2fe7e5bc5028b9a
chirchib <dmitry.kurochkin66@gmail.com>
2020-06-28 22:11:10 +0300

    Добавьте файлы проекта.

---
+++ Game.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    class Game
+    {
+        private string[,] Field = new string[3, 3];
+        public Player FirstPlayer;
+        public Player SecondPlayer;
+        public Player ComputerPlayer;
+
+        public string Result { get; set; }
+
+        public Game()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Field[i, j] = "*";
+                }
+            }
+        }
+
+        public void Start()
+        {
+            this.FirstPlayer = new Player("0");
+            this.SecondPlayer = new Player("X");
+
+            this.EndlessCycle();
+        }
+
+        private void EndlessCycle()
+        {
+            while (CheckOfResult())
+            {
+                this.FirstUpdate();
+                this.SecondUpdate();
+            }
+            Console.WriteLine(Result);
+        }
+
+        public void FirstUpdate()
+        {
+            this.Draw();
+            
+            do
+            {
+                Console.WriteLine("First Player: Enter coordinates:\nOx from 0 to 2");
+                FirstPlayer.Ox = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Oy from 0 to 2"); 
+                FirstPlayer.Oy = Convert.ToInt32(Console.ReadLine());
+            } while ((FirstPlayer.Ox > 2 || FirstPlayer.Ox < 0) && (FirstPlayer.Oy > 2 || FirstPlayer.Oy < 0));
+
+            Field[FirstPlayer.Ox, FirstPlayer.Oy] = FirstPlayer.Skin;
+        }
+
+        public void SecondUpdate()
+        {
+            this.Draw();
+
+            if (!CheckOfResult())
+            {
+                return;
+            }
+
+            do
+            {
+                Console.WriteLine("Second Player: Enter coordinates:\nOx from 0 to 2");
+                SecondPlayer.Ox = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Oy from 0 to 2");
+                SecondPlayer.Oy = Convert.ToInt32(Console.ReadLine());
+            } while ((SecondPlayer.Ox > 2 || SecondPlayer.Ox < 0) && (SecondPlayer.Oy > 2 || SecondPlayer.Oy < 0));
+
+            Field[SecondPlayer.Ox, SecondPlayer.Oy] = SecondPlayer.Skin;
+        }
+
+        public void Draw()
+        {
+
+            Console.Clear();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(Field[i,j], " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public bool CheckOfResult()
+        {
+            if (CheckOfResultFirstPlayer() == false
+            || CheckOfResultSecondPlayer() == false
+            || CheckOfDraw() == false)
+            {
+                return false;
+            }
+            else return true;
+        }
+
+        public bool CheckOfResultFirstPlayer()
+        {
+            if ((Field[0,2] == Field[1,1] && Field[1,1] == Field[2,0] && Field[1,1] == FirstPlayer.Skin)
+                || (Field[0, 0] == Field[1, 1] && Field[1, 1] == Field[2, 2] && Field[1, 1] == FirstPlayer.Skin))
+            {
+                Result = "FIRST PLAYER WON !!!";
+                return false;
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if ((Field[i, 0] == Field[i, 1] && Field[i,1] == Field[i, 2] && Field[i, 2] == FirstPlayer.Skin)
+                        || (Field[0, j] == Field[1, j] && Field[1, j] == Field[2, j] && Field[2, j] == FirstPlayer.Skin))
+                    { 
+                        Result = "FIRST PLAYER WON !!!";
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public bool CheckOfResultSecondPlayer()
+        {
+            if ((Field[0, 2] == Field[1, 1] && Field[1, 1] == Field[2, 0] && Field[1, 1] == SecondPlayer.Skin)
+                || (Field[0, 0] == Field[1, 1] && Field[1, 1] == Field[2, 2] && Field[1, 1] == SecondPlayer.Skin))
+            {
+                Result = "SECOND PLAYER WON !!!";
+                return false;
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if ((Field[i, 0] == Field[i, 1] && Field[i, 1] == Field[i, 2] && Field[i, 2] == SecondPlayer.Skin)
+                        || (Field[0, j] == Field[1, j] && Field[1, j] == Field[2, j] && Field[2, j] == SecondPlayer.Skin))
+                    {
+                        Result = "SECOND PLAYER WON !!!";
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public bool CheckOfDraw()
+        {
+            if (CheckOfResultFirstPlayer() && CheckOfResultSecondPlayer())
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if ( Field[i,j] != FirstPlayer.Skin || Field[i, j] != SecondPlayer.Skin)
+                        {
+                            return true;
+                        }
+                    }
+                }
+                Result = "IT'S DRAW !!!";
+                return false;
+            }
+        return true;
+        }
+
+
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    class Player
+    {
+        public string Skin;
+        public int Scores;
+        public int Ox { get; set; }
+                
+        public int Oy { get; set; }
+
+        public Player(string Symbol)
+        {
+            this.Skin = Symbol;
+        }    
+    }
+}
